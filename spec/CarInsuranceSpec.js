4644f428272a394064bb7e9eebd9418dbb4343a9
Jose Manuel Salazar <jmsg78@gmail.com>
2020-10-25 00:38:25 -0500

    create test CarInsurance Class

---
+++ spec/CarInsuranceSpec.js
@@ -0,0 +1,21 @@
+const expect = require('chai').expect;
+
+const { Product } = require('../src/coTest');
+const { CarInsurance } = require('../src/CarInsurance');
+
+describe("Create a New Product", function() {
+
+  it("should get a new Product", function() {
+    const productList = new CarInsurance([ new Product("Medium Product", 2, 3) ]);
+    const products = productList.products;
+    products.should.be.a('array');
+  });
+
+    it("should get a new array empty", function() {
+    const productList = new CarInsurance();
+    const products = productList.products;
+    products.should.be.a('array');
+  });
+
+});
+
