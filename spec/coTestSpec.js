c04c4b2f8f35b124c06436e24b7e6e163de9e027
Jose Manuel Salazar <jmsg78@gmail.com>
2020-10-25 00:23:12 -0500

    install environment coverage test and initial test

--- spec/coTestSpec.js
+++ spec/coTestSpec.js
@@ -1,15 +1,33 @@
 const expect = require('chai').expect;
 
-const coTest = require('../src/coTest');
-const CarInsurance = coTest.CarInsurance;
-const Product = coTest.Product;
+const { Product } = require('../src/coTest');
+const { CarInsurance } = require('../src/CarInsurance');
 
-describe("Co Test", function() {
+describe("Create a New Product", function() {
 
-  it("should foo", function() {
-    const coTest = new CarInsurance([ new Product("foo", 0, 0) ]);
+  it("should create a new Product #1 called Medium Product", function() {
+    const coTest = new CarInsurance([ new Product("Medium Product", 2, 3) ]);
     const products = coTest.updatePrice();
-    expect(products[0].name).equal("foo");
+    expect(products[0].name).equal("Medium Product");
+  });
+
+    it("should create a new Product #2 when price is maxPrice", function() {
+    const productList = new CarInsurance([ new Product("Special Full Coverage",12, 50) ]);
+    const products = productList.updatePrice();
+    expect(products[0].price).equal(49);
+  });
+
+it("should create a new Product #3 when no more days left", function() {
+    const productList = new CarInsurance([ new Product("Special Full Coverage",0, 50) ]);
+    const products = productList.updatePrice();
+    expect(products[0].price).equal(49);
+  });
+
+  it("should create a new Product #4 when no more days left and price is 0", function() {
+    const productList = new CarInsurance([ new Product("Special Full Coverage",0, 0) ]);
+    const products = productList.updatePrice();
+    expect(products[0].price).equal(0);
   });
 
 });
+
