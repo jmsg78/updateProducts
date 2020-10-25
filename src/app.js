0053bdd33e0c9a60dd3f9db33d8e0838c6cd3af6
Jose Manuel Salazar <jmsg78@gmail.com>
2020-10-24 20:41:41 -0500

    update code function productPrinter

--- src/app.js
+++ src/app.js
@@ -13,9 +13,7 @@ const productsAtDayZero = [
 ];
 
 const carInsurance = new CarInsurance(productsAtDayZero);
-const productPrinter = function (product) {
-  console.log(`${product.name}, ${product.sellIn}, ${product.price}`);
-};
+const productPrinter = (product) =>  console.log(`${product.name}, ${product.sellIn}, ${product.price}`);
 
 for (let i = 1; i <= 30; i += 1) {
   console.log(`Day ${i}`);
